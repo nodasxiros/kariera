8bbac9a04660c7aba99908440860c790d350044d
Nondas Xiros <nodasxiros@gmail.com>
2021-07-15 12:36:59 +0300

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+function initSwagger(app) {
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Kariera API')
+    .setDescription('This the Restful API for the kariera challenge')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  initSwagger(app)
   await app.listen(3000);
 }
 bootstrap();
