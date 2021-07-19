86071122c4905e1da5f69244254764d2f637525d
Nondas Xiros <nodasxiros@gmail.com>
2021-07-19 09:05:44 +0300

    Api response and error handling for company repo

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,8 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-import { ApiTags, ApiBearerAuth, ApiNoContentResponse, ApiQuery } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiQuery,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 import { InsertResult } from 'typeorm';
 
 @ApiBearerAuth()
@@ -12,31 +30,60 @@ export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Post()
+  @ApiCreatedResponse({ status: 201, description: 'Created'})
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request'})
   async create(@Body() createCompanyDto: CreateCompanyDto): Promise<InsertResult> {
     return await this.companiesService.create(createCompanyDto);
   }
 
   @Get()
-  @ApiQuery({ name: 'where', required: false })
+  @ApiOkResponse({ status: 200, description: 'Result' })
+  @ApiNotFoundResponse({ status: 404, description: 'Not found' })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request'})
+  @ApiQuery({
+    name: 'where',
+    required: false,
+    schema: {
+      type: 'string',
+      example: '{ "id": 1 }'
+    }
+  })
   async findAll(
-    @Query('where') where: object = {},
+    @Query('where') where: string = '{}',
   ) {
-    return await this.companiesService.findAll({
-      where
+    const res = await this.companiesService.findAll({
+      where: JSON.parse(where)
     });
+    if (!res.length) throw new NotFoundException({
+      status: 404,
+      message: 'No results with these criteria'
+    });
+    return res;
   }
 
   @Get(':id')
+  @ApiOkResponse({ status: 200, description: 'Result' })
+  @ApiNotFoundResponse({ status: 404, description: 'Not found' })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request'})
   findOne(@Param('id') id: string) {
-    return this.companiesService.findOne(+id);
+    const res = this.companiesService.findOne(+id);
+    if (!res) throw new NotFoundException({
+      status: 404,
+      message: 'No results with these criteria'
+    });
+    return res;
   }
 
   @Patch(':id')
+  @ApiOkResponse({ status: 200, description: 'Company has been updated' })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request'})
   update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
     return this.companiesService.update(+id, updateCompanyDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ status: 200, description: 'Company has been updated' })
+  @ApiBadRequestResponse({ status: 400, description: 'Bad request'})
   remove(@Param('id') id: string) {
     return this.companiesService.remove(+id);
   }
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,22 +12,62 @@ export class CompaniesService {
     private companyRepository: Repository<Company>
   ) {}
   create(createCompanyDto: CreateCompanyDto): Promise<InsertResult> {
-    return this.companyRepository.insert(createCompanyDto);
+    return this.companyRepository
+      .insert(createCompanyDto)
+      .catch(err => {
+        throw new BadRequestException({
+          name: err.name,
+          message: err.message,
+          detail: err.detail,
+        });
+      });
   }
 
-  findAll(options?: any): Promise<Company[]> {
-    return this.companyRepository.find(options);
+  findAll(options?: FindManyOptions): Promise<Company[]> {
+    return this.companyRepository
+      .find(options)
+      .catch(err => {
+        throw new BadRequestException({
+          name: err.name,
+          message: err.message,
+          detail: err.detail,
+        });
+      });
   }
 
   findOne(id: number): Promise<Company> {
-    return this.companyRepository.findOne(+id);
+    return this.companyRepository
+      .findOne(+id)
+      .catch(err => {
+        throw new BadRequestException({
+          name: err.name,
+          message: err.message,
+          detail: err.detail,
+        });
+      });
   }
 
   update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return this.companyRepository.update(+id, updateCompanyDto);
+    return this.companyRepository
+      .update(+id, updateCompanyDto)
+      .catch(err => {
+        throw new BadRequestException({
+          name: err.name,
+          message: err.message,
+          detail: err.detail,
+        });
+      });
   }
 
   remove(id: number) {
-    return this.companyRepository.softDelete(+id);
+    return this.companyRepository
+      .softDelete(+id)
+      .catch(err => {
+        throw new BadRequestException({
+          name: err.name,
+          message: err.message,
+          detail: err.detail,
+        });
+      });
   }
 }
